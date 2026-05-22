0edcf3f043cd2d5395b527a1c09bda7bfeb6bc10
Liudmila Siamionava <lucy.semenova@gmail.com>
2026-05-22 14:50:10 +0200

    fix API_BASE_URL

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://events-startup-project-app.onrender.com/";
+  import.meta.env.VITE_API_BASE_URL || "https://events-startup-project-api.onrender.com/";
 
 export default function api(route) {
   return `${API_BASE_URL}/api${route}`;
